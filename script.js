3c561e2dcca8ed5ee07deda709e1fbabc4a91c3a
Sujit Akulwar <sujitakulwar@gmail.com>
2022-07-31 18:43:35 +0530

    3

---
+++ script.js
@@ -0,0 +1,28 @@
+var winner = document.getElementById("winner");
+var image1 = document.getElementById("player1");
+var image2 = document.getElementById("player2");
+var dice1 = 1;
+var dice2 = 1;
+function roll(){
+    get_number();
+    set_images();
+}
+
+function get_number(){
+    dice1 = Math.floor(Math.random()*6);
+    dice2 = Math.floor(Math.random()*6);
+}
+
+function set_images(){
+    put_image(dice1,image1);
+    put_image(dice2,image2);
+}
+
+function put_image(dice,img){
+    if(dice == 0){img.style.backgroundImage = "url('Alea_1.png')";}
+    else if(dice == 1){img.style.backgroundImage = "url('Alea_2.png')";}
+    else if(dice == 2){img.style.backgroundImage = "url('Alea_3.png')";}
+    else if(dice == 3){img.style.backgroundImage = "url('Alea_4.png')";}
+    else if(dice == 4){img.style.backgroundImage = "url('Alea_5.png')";}
+    else {img.style.backgroundImage = "url('Alea_6.png')";}
+}
